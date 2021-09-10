f5a39a0946c683a2502020e54b80d1e614ec04d6
Kat Athay <89493136+Kat4Kimi@users.noreply.github.com>
2021-09-10 11:15:28 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'patients#index'
+
+  resources :doctors  
+  resources :patients do
+    resources :appointments, only: [:index, :new, :create, :destroy]
+  end
+end
+
+
+
